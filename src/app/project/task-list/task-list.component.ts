09782e18baf7c87fbe4c6b4a552cf45dc65eacd8
benmuratseker <benmuratseker@hotmail.com>
2024-12-24 23:33:55 +0100

    task list converted to use dynamic data

--- src/app/project/task-list/task-list.component.ts
+++ src/app/project/task-list/task-list.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { Task } from '../../task.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.css'
+  styleUrl: './task-list.component.css',
 })
 export class TaskListComponent {
+  tasks: Task[] = [
+    {
+      id: 1,
+      name: 'Design wireframe',
+      description: '',
+      completed: false,
+      dueDate: new Date('2025-01-15'),
+      project: 1,
+    },
+    {
+      id: 2,
+      name: 'Develop frontend',
+      description: '',
+      completed: true,
+      dueDate: new Date('2025-01-05'),
+      project: 1,
+    },
+    {
+      id: 3,
+      name: 'Develop backend',
+      description: '',
+      completed: false,
+      dueDate: new Date('2025-01-10'),
+      project: 1,
+    },
+  ];
 
+  handleCheckbox(id: number){
+    console.log(id);
+    const taskIndex = this.tasks.findIndex((task) => task.id == id);
+    this.tasks[taskIndex].completed = !this.tasks[taskIndex].completed;
+  }
 }
