3fab42a3b991893ace8889b07856852cf1a78cfc
benmuratseker <benmuratseker@hotmail.com>
2024-12-25 01:14:54 +0100

    task service implemented

--- src/app/project/task-list/task-list.component.ts
+++ src/app/project/task-list/task-list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Task } from '../../task.model';
 import { DatePipe } from '@angular/common';
+import { TaskService } from '../../task.service';
 
 @Component({
   selector: 'app-task-list',
@@ -10,36 +11,49 @@ import { DatePipe } from '@angular/common';
   styleUrl: './task-list.component.css',
 })
 export class TaskListComponent {
-  tasks: Task[] = [
-    {
-      id: 1,
-      name: 'Design wireframe',
-      description: '',
-      completed: false,
-      dueDate: new Date('2025-01-15'),
-      project: 1,
-    },
-    {
-      id: 2,
-      name: 'Develop frontend',
-      description: '',
-      completed: true,
-      dueDate: new Date('2025-01-05'),
-      project: 1,
-    },
-    {
-      id: 3,
-      name: 'Develop backend',
-      description: '',
-      completed: false,
-      dueDate: new Date('2025-01-10'),
-      project: 1,
-    },
-  ];
+  
+  //dummy data for dynamic data load
+  // tasks: Task[] = [
+  //   {
+  //     id: 1,
+  //     name: 'Design wireframe',
+  //     description: '',
+  //     completed: false,
+  //     dueDate: new Date('2025-01-15'),
+  //     project: 1,
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Develop frontend',
+  //     description: '',
+  //     completed: true,
+  //     dueDate: new Date('2025-01-05'),
+  //     project: 1,
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'Develop backend',
+  //     description: '',
+  //     completed: false,
+  //     dueDate: new Date('2025-01-10'),
+  //     project: 1,
+  //   },
+  // ];
+
+  tasks: Task[] =[];
+  
+  private taskService = inject(TaskService);
+
+  constructor() {
+    this.tasks = this.taskService.getTasks();
+  }
 
   handleCheckbox(id: number){
-    console.log(id);
+    //console.log(id);
     const taskIndex = this.tasks.findIndex((task) => task.id == id);
-    this.tasks[taskIndex].completed = !this.tasks[taskIndex].completed;
+    const updatedTask = this.tasks[taskIndex];
+    updatedTask.completed = !updatedTask.completed
+    //this.tasks[taskIndex].completed = !this.tasks[taskIndex].completed;
+    this.tasks = this.taskService.updateTask(updatedTask);
   }
 }
--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Task } from './task.model';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class TaskService {
 
   tasks: Task[] = [
