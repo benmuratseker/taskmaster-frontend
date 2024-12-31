7eb48677343b81f451160b74f7cc5c5b58f03eb6
benmuratseker <benmuratseker@hotmail.com>
2025-01-01 00:14:07 +0100

    user menu added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { ProjectComponent } from "./project/project.component";
+import { AuthService } from './Services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProjectComponent],
+  imports: [RouterOutlet, ProjectComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'taskmaster-frontend';
+  authService = inject(AuthService);
+  
+  logout(){
+    this.authService.logout();
+  }
 }
